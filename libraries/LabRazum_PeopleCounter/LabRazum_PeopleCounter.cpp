bd61e1cb42782dfea7e8fb3565f735ec01450bd3
Viktor Osin <viva-os@mail.ru>
2020-05-27 00:32:31 +0600

    Протестирована и добавлена защита от помех

--- libraries/LabRazum_PeopleCounter/LabRazum_PeopleCounter.cpp
+++ libraries/LabRazum_PeopleCounter/LabRazum_PeopleCounter.cpp
@@ -6,29 +6,32 @@ Room::Room(byte pinSensor1, byte pinSensor2, byte pinRelay)
     _pinSensor1 = pinSensor1;
     _pinSensor2 = pinSensor2;
     _pinRelay = pinRelay;
+    _handModeInit = 0;
+    _photoSensorInit = 0;
 }
 
 
 Room::Room(byte pinSensor1, byte pinSensor2, byte pinRelay, byte pinHandMode)
 {   
-    #define HAND_MODE_BUTTON_CONNECT
     _pinSensor1 = pinSensor1;
     _pinSensor2 = pinSensor2;
     _pinHandMode = pinHandMode;
     _pinRelay = pinRelay;
+    _handModeInit = 1;
+    _photoSensorInit = 0;
 }
 
 
 Room::Room(byte pinSensor1, byte pinSensor2, byte pinRelay, byte pinHandMode, byte pinPhotoSensor, int photoSensorThreshold)
 {  
-    #define PHOTO_SENSOR_CONNECT
-    #define HAND_MODE_BUTTON_CONNECT 
     _pinSensor1 = pinSensor1;
     _pinSensor2 = pinSensor2;
     _pinHandMode = pinHandMode;
     _pinRelay = pinRelay;
     _pinPhotoSensor = pinPhotoSensor;
     _photoSensorThreshold = photoSensorThreshold;
+    _handModeInit = 1;
+    _photoSensorInit = 1;
 }
 
 void Room::begin()
@@ -38,8 +41,16 @@ void Room::begin()
     pinMode(_pinRelay, OUTPUT);
     digitalWrite(_pinRelay, LOW);
 
+  if(_handModeInit == 1)
+  {
     pinMode(_pinHandMode, INPUT);
+  }
+  
+  if(_photoSensorInit== 1)
+  {
     pinMode(_pinPhotoSensor, INPUT);
+  }
+  
 }
 void Room::debug(bool onDebug)
 {
@@ -51,13 +62,56 @@ byte Room::count_people_val()
 {
   _IRsensor1 = digitalRead(_pinSensor1);
   _IRsensor2 = digitalRead(_pinSensor2);
+
+  //фильтрация помех датчика №1 - при помехе (старт/остановка двигателя, включение/отключение мощной нагрузки) 
+  //сигнал датчика переключается на 3-5мс и восстанавливается обратно, поэтому ставим таймер на 10 мс (с запасом)
+  if((_IRsensor1 == 0) && (_timerIRsensor1On == 0)) //если пришел сигнал с датчика и таймер помехи не был запущен
+  {
+      _timerIRsensor1On = 1; //запускаем таймер и ставим флаг запуска
+      timerIRsensor1(1);
+  }
+
+  if((_IRsensor1 == 1) && (_timerIRsensor1On == 1)) //если во время работы  таймера (10 мс), датчик меняет состояние, значит это была помеха 
+  {
+        timerIRsensor1(0); //останавливаем 
+        _timerIRsensor1On = 0;
+  }
+
+  if((_IRsensor1 == 0) && (timerIRsensor1(2) >= 10)) //если сигнал "держится" на нуле больше 10 мс
+  {
+      _signalSensorGood = 1; //говорим, что сигнал "хороший"
+      timerIRsensor1(0);     //останавливаем таймер
+      _timerIRsensor1On = 0;
+  }
+
+  //фильтрация помех датчика №2
+  if((_IRsensor2 == 0) && (_timerIRsensor2On == 0)) //если пришел сигнал с датчика и таймер помехи не был запущен
+  {
+      _timerIRsensor2On = 1; //запускаем таймер и ставим флаг запуска
+      timerIRsensor2(1);
+  }
+
+  if((_IRsensor2 == 1) && (_timerIRsensor2On == 1)) //если во время работы  таймера помехи (10 мс), датчик меняет состояние, значит это была помеха 
+  {
+        timerIRsensor2(0); //останавливаем таймер
+        _timerIRsensor2On = 0;
+  }
+
+  if((_IRsensor2 == 0) && (timerIRsensor2(2) >= 10)) //если сигнал "держится" на нуле больше 10 мс
+  {
+      _signalSensorGood = 1; //говорим, что сигнал "хороший"
+      timerIRsensor2(0);     //останавливаем таймер
+      _timerIRsensor2On = 0;
+  }
+
+
   if(_onDebug == 1)
   {
     Serial.print("IR_sensor_1:" + String(_IRsensor1) + " IR_sensor_2:" + String(_IRsensor2));
   }
   
   //определяем, какой датчик сработал первым
-  if((_firstSensor2 == 0) && (_firstSensor1 == 0))
+  if((_firstSensor2 == 0) && (_firstSensor1 == 0) && (_signalSensorGood == 1))
   {
     if((_IRsensor1 == 0) && (_IRsensor2 == 1)) 
     {  
@@ -91,6 +145,7 @@ void Room::valuePeopleChange()
         {
           _valuePeople++;
           _firstSensor1 = 0;
+          _signalSensorGood = 0;
         }
       }
       if(_firstSensor2)
@@ -99,6 +154,7 @@ void Room::valuePeopleChange()
         {
           _valuePeople--;
           _firstSensor2 = 0;
+          _signalSensorGood = 0;
         }
       }
     }
@@ -106,6 +162,7 @@ void Room::valuePeopleChange()
     {
        _firstSensor1 = 0;
        _firstSensor2 = 0;
+       _signalSensorGood = 0;
     }         
   }
 }
@@ -116,34 +173,89 @@ void Room::start_timer_people()
     _timerPeople = 0;
 }
 
+byte Room::timerIRsensor1(byte mode)
+{
+  switch (mode)
+  {
+    case 0: //сброс счетчика
+    _valTimerIRsensor1 = 0;
+    return 0;
+    break; 
+    
+    case 1: //старт счетчика - запоминаем, чему был равен millis()
+    _oldTimerIRsensor1 = millis();
+    _valTimerIRsensor1 = 0;
+    return 1;
+    break;
+
+    case 2: //показания счетчика
+    _valTimerIRsensor1 = millis() - _oldTimerIRsensor1;
+    return _valTimerIRsensor1;
+    break;
+  }
+}
+
+byte Room::timerIRsensor2(byte mode)
+{
+  switch (mode)
+  {
+    case 0: //сброс счетчика
+    _valTimerIRsensor2 = 0;
+    return 0;
+    break; 
+
+    case 1: //старт счетчика - запоминаем, чему был равен millis()
+    _oldTimerIRsensor2 = millis();
+    _valTimerIRsensor2 = 0;
+    return 1;
+    break;
+
+    case 2: //показания счетчика
+    _valTimerIRsensor2 = millis() - _oldTimerIRsensor2;
+    return _valTimerIRsensor2;
+    break;
+  }
+}
+
+
 byte Room::lightControl()
 {
-    #ifdef HAND_MODE_BUTTON_CONNECT
-    if(digitalRead(_pinHandMode) == 0) //если с кнопки пришел 0, включаем "ручной" режим
-    {
-        lightRoom(0);     //выключить свет в этой комнате
-        _valuePeople = 0; //обнулить счетчик людей
-    } 
-    #endif
-
-    #ifdef HAND_MODE_BUTTON_CONNECT
-    if(digitalRead(_pinHandMode) == 1) //если с кнопки пришла 1, включаем автоматический режим
-    {
-    #endif
-      if (count_people_val() > 0)  //если есть люди в комнате
-      {
-          lightRoom(1);    //включить свет в этой комнате
-      }
-      else
-      {
-          lightRoom(0);    //выключить свет в этой комнате
-          _valuePeople = 0;
-      }
-      
-      return _valuePeople;
-    #ifdef HAND_MODE_BUTTON_CONNECT
-    }   
-    #endif
+     if(_handModeInit == 1)
+     {
+        if(digitalRead(_pinHandMode) == 0) //если с кнопки пришел 0, включаем "ручной" режим
+        {
+            lightRoom(0);     //выключить свет в этой комнате
+            _valuePeople = 0; //обнулить счетчик людей
+        }
+        else
+        {
+          if (count_people_val() > 0)  //если есть люди в комнате
+          {
+              lightRoom(1);    //включить свет в этой комнате
+          }
+          else
+          {
+              lightRoom(0);    //выключить свет в этой комнате
+              _valuePeople = 0;
+          }
+          
+          return _valuePeople;
+        }
+     }
+     else
+     {
+          if (count_people_val() > 0)  //если есть люди в комнате
+          {
+              lightRoom(1);    //включить свет в этой комнате
+          }
+          else
+          {
+              lightRoom(0);    //выключить свет в этой комнате
+              _valuePeople = 0;
+          }          
+          return _valuePeople;
+     }
+       
 }
 
 byte Room::getCountPeople()
@@ -155,35 +267,37 @@ byte Room::getCountPeople()
 void Room::lightRoom(bool lightOn)
 {
     _lightOn = lightOn;
-    #ifdef PHOTO_SENSOR_CONNECT
-    int sunlightValue = analogRead(_pinPhotoSensor);
+    if(_photoSensorInit == 1)
+    {
+      int sunlightValue = analogRead(_pinPhotoSensor);
+      if(_onDebug == 1)
+      {
+        Serial.print(" PhotoSensor:" + String(sunlightValue));
+        if(_darkLight == 1)
+            Serial.println(" Light in Room: "+ String(_lightOn));
+        else
+            Serial.println(" Light in Room: 0");
+      }
 
-    if(sunlightValue > _photoSensorThreshold)
-    {
-        _darkLight = 1;
-    }
-    
-    if(sunlightValue < (_photoSensorThreshold-100))
-    {
-        _darkLight = 0;
-        digitalWrite(_pinRelay, 0);
-    }
+      if(sunlightValue > _photoSensorThreshold)
+      {
+          _darkLight = 1;
+      }
+      
+      if(sunlightValue < (_photoSensorThreshold-100))
+      {
+          _darkLight = 0;
+          digitalWrite(_pinRelay, 0);
+      }
 
-    if(_darkLight == 1)
-    {
-        digitalWrite(_pinRelay, _lightOn);
-    }
-    #endif
-    #ifndef PHOTO_SENSOR_CONNECT
-         digitalWrite(_pinRelay, _lightOn);
-    #endif
-    if(_onDebug == 1)
-    {
-      Serial.print(" PhotoSensor:" + String(sunlightValue));
       if(_darkLight == 1)
-          Serial.println(" Light in Room: "+ String(_lightOn));
-      else
-          Serial.println(" Light in Room: 0");
+      {
+          digitalWrite(_pinRelay, _lightOn);
+      }
+    }
+    else
+    {
+      digitalWrite(_pinRelay, _lightOn);
     }
 }
 
--- libraries/LabRazum_PeopleCounter/LabRazum_PeopleCounter.h
+++ libraries/LabRazum_PeopleCounter/LabRazum_PeopleCounter.h
@@ -16,12 +16,14 @@ class Room
 
     private:
         void start_timer_people();          //таймер на 500 мс для задержки между срабатываниями датчиков
+        byte timerIRsensor1(byte mode);     //тамеры на 10 мс для защиты от ложных срабатываний - помех
+        byte timerIRsensor2(byte mode);
         byte count_people_val();            //метод определяет очередность срабатывания датчиков и выдает количество людей в комнате
         void lightRoom(bool lightOn);       //метод изменяет значение количества людей в комнате, если оно изменилось
         void valuePeopleChange();           //меняет значение счетчика людей, если количество человек в комнате изменилось 
-        byte _pinSensor1, _pinSensor2, _pinHandMode, _pinRelay, _pinPhotoSensor; 
-        bool _firstSensor1, _firstSensor2, _IRsensor1, _IRsensor2, _darkLight, _lightOn, _onDebug;
-        unsigned long _timerPeople, _oldTimerPeople;
+        byte _pinSensor1, _pinSensor2, _pinHandMode, _pinRelay, _pinPhotoSensor, _handModeInit, _photoSensorInit; 
+        bool _firstSensor1, _firstSensor2, _IRsensor1, _IRsensor2, _timerIRsensor1On, _timerIRsensor2On, _darkLight, _lightOn, _onDebug, _signalSensorGood;
+        unsigned long _timerPeople, _oldTimerPeople, _valTimerIRsensor1, _valTimerIRsensor2, _oldTimerIRsensor1, _oldTimerIRsensor2;
         byte _valuePeople;
         int _photoSensorThreshold;       
 };
